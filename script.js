08d3dcba0aa7039f5ec400e58795ce8fe83ae520
Prachi Gujar <prachigujar@Prachis-MacBook-Air.local>
2020-11-16 21:48:59 -0500

    base files including pseudocode

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-$.ajax({
-    url: 'https://developers.zomato.com/api/v2.1/search',
-    method: 'GET',
-    dataType: 'json',
-    data: {
-        apikey: '51229140792268d47f96f56aabbde055',
-    }
-}).then(
-    function(res){
-        console.log(res);
-    }    
-);
+// $.ajax({
+//     url: 'https://developers.zomato.com/api/v2.1/search',
+//     method: 'GET',
+//     dataType: 'json',
+//     data: {
+//         apikey: '51229140792268d47f96f56aabbde055',
+//     }
+// }).then(
+//     function(res){
+//         console.log(res);
+//     }    
+// );
+
+// MVP: Ask the users to select the city, Cuisine type, Dine-in/takeout/delivery and based on the selection we provide the recommendations and details for the restaurant including the rating, location etc.
+
+// create a namespace
+// declare varibales for the selection parameters listed above
+// call the API
+    // callback function for API to show the results using the variables stored earlier
+// create an array to store the results of the API call
+// display the results in grid, link the images with details/information 
+
+
+// STRETCH:
+
+// While displaying the results, give the user an option to reserve a table using the calendar function for a particular date,time, no. of guests  
+// Check the weather for the day when we display the calendar (cloudy/rainy/sunny/snowy day)
+// Create an alert email/sends an alert notification, ex: 2 Days before the reservation 
