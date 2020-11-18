34c9a561f92eef291d8043eb25a74abe26248829
Prachi Gujar <prachigujar@Prachis-MacBook-Air.local>
2020-11-18 17:43:52 -0500

    changed the radius for thesections

--- script.js
+++ script.js
@@ -24,16 +24,16 @@ bookingApp.categories ={
     delivery: 1,
     takeout: 5
 }
-// const selections = [];
-// console.log(selections);
 
-bookingApp.getRecommendation = function (selectedCityId,selectedCuisineId, selectedCategoryId){
+
+bookingApp.getRecommendation = function (selectedCityId, selectedCuisineId, selectedCategoryId){
     $.ajax({
         url: bookingApp.zomatoURL,
         method: 'GET',
         dataType: 'json',
         data: {
             apikey: '51229140792268d47f96f56aabbde055',
+            count: 20,
             entity_id: selectedCityId,
             entity_type: 'city',
             cuisines: selectedCuisineId,
@@ -44,37 +44,49 @@ bookingApp.getRecommendation = function (selectedCityId,selectedCuisineId, selec
     })
 };
 
+// create an array for the cuisines , cities, prefer.
+//forEach loop so it selects only one value for each and stores it in the selected variable
 
 // Need a method to call the getRecommendation which will take city id as an argument
+// $('button').on('click',function(){
+    // console.log('clicked!');
+    // create a method to hold an event listenere which will listen for everytime the user selects a new city from the selections
+    let cityId;
+    $('.cities').on('click', "label", function () {
+        const selectedCity = $(this).attr("for");
+        cityId = bookingApp.cities[selectedCity];
+        console.log(cityId);
+        // selections.push(cityId); 
+        // gives the city_id  
+        // To use: this calls the function for he selected city ID: bookingApp.getRecommendation(cityId);
+    })
 
     // create a method to hold an event listenere which will listen for everytime the user selects a new city from the selections
+    let cuisineId;
+    $('.cuisine').on('click', "label", function () {
+        const selectedCuisine = $(this).attr("for");
+        cuisineId = bookingApp.cuisinesAvailable[selectedCuisine];
+        console.log(cuisineId);
+        // selections.push(cuisineId);
+        // gives the city_id  
+        // To use: this calls the function for he selected city ID: bookingApp.getRecommendation(cityId);
+    })
 
-$('.cities').on('click', "label", function(){
-    const selectedCity = $(this).attr("for");
-    const cityId = bookingApp.cities[selectedCity]; 
-    console.log(cityId);
-    // selections.push(cityId); 
-    // gives the city_id  
-    // To use: this calls the function for he selected city ID: bookingApp.getRecommendation(cityId);
-})
+    // create a method to hold an event listenere which will listen for everytime the user selects a new city from the selections
+    let categoryId;
+    $('.preferences').on('click', "label", function () {
+        const selectedCategory = $(this).attr("for");
+        categoryId = bookingApp.categories[selectedCategory];
+        console.log(categoryId);
+        // selections.push(cuisineId);
+        // gives the city_id  
+        // To use: this calls the function for he selected city ID: bookingApp.getRecommendation(cityId);
+    });
 
-$('.cuisine').on('click', "label", function () {
-    const selectedCuisine = $(this).attr("for");
-    const cuisineId = bookingApp.cuisinesAvailable[selectedCuisine];
-    console.log(cuisineId);
-    // selections.push(cuisineId);
-    // gives the city_id  
-    // To use: this calls the function for he selected city ID: bookingApp.getRecommendation(cityId);
-})
-
-$('.preferences').on('click', "label", function () {
-    const selectedCategory = $(this).attr("for");
-    const categoryId = bookingApp.categories[selectedCategory];
-    console.log(categoryId);
-    // selections.push(cuisineId);
-    // gives the city_id  
-    // To use: this calls the function for he selected city ID: bookingApp.getRecommendation(cityId);
-})
+    // console.log(bookingApp.getRecommendation(cityId, cuisineId, categoryId));
+    
+// });
+    
 
 
 
@@ -100,10 +112,10 @@ $('.preferences').on('click', "label", function () {
 // Create an alert email/sends an alert notification, ex: 2 Days before the reservation 
   $(document).ready(function() {
 
-
-      $('button').on('click', function (e) {
-          e.preventDefault();
-          console.log("clicked");
-          $('input').removeAttr('checked');
-      });
+    //   $('.options').on('click','.submit', function (e) {
+    //       e.preventDefault();
+    //       console.log("clicked");
+    //       const checkBox = $(this).find('.srOnly');
+    //       checkBox.removeAttr('checked');
+    //   });
   });
