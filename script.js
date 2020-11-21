5abf5b3f155ae6c711dde31902a7c37a53b2e7b8
Martin <martin.nguyen.20289@gmail.com>
2020-11-20 19:22:09 -0500

    updated animations

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const bookingApp = {};
+const bookingApp = {}
 bookingApp.zomatoURL = 'https://developers.zomato.com/api/v2.1/search';
 
 // cities.......
@@ -9,21 +9,21 @@ bookingApp.cities = {
     calgary: 300,
     montreal: 294,
     ottawa: 295
-};
+}
 bookingApp.cityLabel=[
-    $('[for="toronto"]'),
-    $('[for="vancouver"]'),
-    $('[for="calgary"]'),
-    $('[for="ottawa"]'),
-    $('[for="montreal"]'),
-];
+    $('[for="toronto"] h3'),
+    $('[for="vancouver"] h3'),
+    $('[for="calgary"] h3'),
+    $('[for="ottawa"] h3'),
+    $('[for="montreal"] h3'),
+]
 bookingApp.cityCheckbox=[
     $('#toronto'),
     $('#vancouver'),
     $('#calgary'),
     $('#ottawa'),
     $('#montreal'),
-];
+]
 
 // cuisine...........
 bookingApp.cuisine = {
@@ -36,17 +36,20 @@ bookingApp.cuisine = {
     bubbleTea: 247,
     other: 381
     // canadian cuisine id for other
-};
+}
+
+
 bookingApp.cuisineLabel = [
-    $('[for="indian"]'),
-    $('[for="pizza"]'),
-    $('[for="chinese"]'),
-    $('[for="sushi"]'),
-    $('[for="middleEastern"]'),
-    $('[for="desserts"]'),
-    $('[for="bubbleTea"]'),
-    $('[for="other"]'),
-];
+    $('[for="indian"] h3'),
+    $('[for="pizza"] h3'),
+    $('[for="chinese"] h3'),
+    $('[for="sushi"] h3'),
+    $('[for="middleEastern"] h3'),
+    $('[for="desserts"] h3'),
+    $('[for="bubbleTea"] h3'),
+    $('[for="other"] h3'),
+]
+
 bookingApp.cuisineCheckbox=[
     $('#indian'),
     $('#pizza'),
@@ -56,35 +59,43 @@ bookingApp.cuisineCheckbox=[
     $('#desserts'),
     $('#bubbleTea'),
     $('#other'),
-];
+]
 
 // Preferences........
 bookingApp.preferences ={
     dineIn: 2,
     delivery: 1,
     takeout: 5
-};
+}
 bookingApp.preferenceLabel = [
-    $('[for="dineIn"]'),
-    $('[for="delivery"]'),
-    $('[for="takeout"]'),
+    $('[for="dineIn"] h3'),
+    $('[for="delivery"] h3'),
+    $('[for="takeout"] h3'),
 
-];
+]
 bookingApp.preferenceCheckbox=[
     $('#dineIn'),
     $('#delivery'),
     $('#takeout'),
-];
+]
 
 
 bookingApp.citiesSelection = $('.cities');
 bookingApp.cuisineSelection = $('.cuisine');
 bookingApp.preferencesSelection = $('.preferences');
 bookingApp.submitButton = $('.submit');
+bookingApp.buttonClicked = false;
+bookingApp.recommendationSection = $('.recommendation')
 bookingApp.recommendationDisplay = $('.recommendationDisplay');
 bookingApp.citiesId;
 bookingApp.cuisineId;
 bookingApp.preferencesId;
+
+// to display recommendation...
+bookingApp.staticDiv = $('.options');
+bookingApp.slidingDiv = $('.recommendation');
+
+bookingApp.slidingDiv.hide();
 
 
 bookingApp.resetStyles = function(element){
@@ -93,23 +104,25 @@ bookingApp.resetStyles = function(element){
         'opacity': 1,
         'border-radius': '0',
         'box-shadow': '0px 0px transparent'
-    })
+    });
 }
+
 bookingApp.highlightStyles = function (element, shadowColor) {
     element.css({
         'background-color': '#E5989B',
         'opacity': 0.7,
         'border-radius': '15px',
         'box-shadow': '5px 5px '+ shadowColor
-    })
+    });
 }
+
 // selection:cities, selectionLabel: bookingApp.citySelection , selectionCheckbox: bookingApp.cityCheckbox
 bookingApp.select = function(jquery, selection, selectionLabel, selectionCheckbox ){
     jquery.on('click', "label", function () {
         const selected = $(this).attr("for");
         selectionLabel.forEach(function (value, index) {
-            if (value[0].htmlFor !== selected) { 
-                selectionCheckbox[index].attr('checked', false); 
+            if (value[0].parentNode.htmlFor !== selected) {
+                selectionCheckbox[index].attr('checked', false);
                 bookingApp.resetStyles(selectionLabel[index]);
             }
             else if (selectionCheckbox[index].attr('checked')) {
@@ -119,19 +132,19 @@ bookingApp.select = function(jquery, selection, selectionLabel, selectionCheckbo
             else {
                 selectionCheckbox[index].attr('checked', true);
                 bookingApp[`${selection}Id`] = bookingApp[selection][selected];
-                if (selection === 'cities'){
+                if (selection === 'cities') {
                     bookingApp.highlightStyles(selectionLabel[index], '#6D6875')
                 }
-                else{
+                else {
                     bookingApp.highlightStyles(selectionLabel[index], '#500808')
                 }
             }
         });
         bookingApp.checkSelection(selection, selectionCheckbox);
-        console.log(`${selection} ID is ${bookingApp[`${selection}Id`]}`)
-    })
+    });
 }
 
+// check if the selection is selected, if not then give an error to the user........
 bookingApp.checkSelection = function(selection, selectionCheckbox) {
     let nothingSelected = true;
     selectionCheckbox.forEach ( function(value) {
@@ -144,6 +157,7 @@ bookingApp.checkSelection = function(selection, selectionCheckbox) {
     }
 }
 
+
 bookingApp.getUserSelections = function() {
   bookingApp.select(bookingApp.citiesSelection, 'cities', bookingApp.cityLabel, bookingApp.cityCheckbox);
   bookingApp.select(bookingApp.cuisineSelection, 'cuisine', bookingApp.cuisineLabel, bookingApp.cuisineCheckbox);
@@ -151,20 +165,49 @@ bookingApp.getUserSelections = function() {
 }
 
 bookingApp.handleButton = function(button, start) {
-    button.on('click', function() {
+  
+    button.on('click', function () {
+      if (!bookingApp.buttonClicked) {
         if (!bookingApp.citiesId) {
-            console.log('Please select a city');
+          console.log('Please select a city');
         }
         if (!bookingApp.cuisineId) {
-            console.log('Please select a cuisine');
+          console.log('Please select a cuisine');
         }
         if (!bookingApp.preferencesId) {
-            console.log('Please select a preference');
+          console.log('Please select a preference');
         }
-        console.log(start);
+        if (bookingApp.citiesId && bookingApp.cuisineId && bookingApp.preferencesId) {
+          bookingApp.buttonClicked = true;
+          bookingApp.getRecommendation(bookingApp.citiesId, bookingApp.cuisineId, bookingApp.preferencesId, start);
+          // Toggle on/off loading animation
+          bookingApp.animations.handleAnimations();
+          bookingApp.handleRecommendationScroll(bookingApp.recommendationSection, start);
+          // display: if else statement for media query: make it in a function and call it here
+          bookingApp.staticDiv.css({
+            'width': '50vw'
+          });
+          bookingApp.slidingDiv.animate({
+            'width': 'calc(50vw - 50px)',
+            'opacity': 'show'
+          }, 500);
+        }
+      }
+    });
+    
+}
+
+bookingApp.handleRecommendationScroll = function(recommendationSection, start) {
+    recommendationSection.on('scroll', () => {
+        if(recommendationSection[0].scrollTop + recommendationSection[0].clientHeight >= recommendationSection[0].scrollHeight) {
+            start += 9;
+            bookingApp.getRecommendation(bookingApp.citiesId, bookingApp.cuisineId, bookingApp.preferencesId, start);
+        }
+    });
+    if (recommendationSection[0].scrollHeight === recommendationSection[0].clientHeight) {
         start += 9;
         bookingApp.getRecommendation(bookingApp.citiesId, bookingApp.cuisineId, bookingApp.preferencesId, start);
-    });
+    }
 }
 
 
@@ -183,8 +226,8 @@ bookingApp.getRecommendation = function (selectedCityId, selectedCuisineId, sele
             category: selectedCategoryId
         }
     }).then(function (recommendationResponse) {
-          bookingApp.processRecommendation(recommendationResponse.restaurants);
-    })
+        bookingApp.processRecommendation(recommendationResponse.restaurants);
+    });
 };
 
 // location, currency, featured image, name, zomato url, userRatings.aggregate_rating
@@ -202,6 +245,7 @@ bookingApp.processRecommendation = function(recommendations) {
     });
 }
 
+// to display the image..........
 bookingApp.appendImage = function(display, image, name, url) {
     display.append(`
         <li>
@@ -211,37 +255,87 @@ bookingApp.appendImage = function(display, image, name, url) {
     `)
 }
 
+
+// init function to call the api function
 bookingApp.init = function() {
     bookingApp.getUserSelections();
     bookingApp.handleButton(bookingApp.submitButton, 0);
 }
 
+
+// calling the API function once the document loads.......
 $(document).ready(() => {
     bookingApp.init();
 });
 
 
-
-bookingApp.animations = {};
+bookingApp.animations = {}
+bookingApp.animations.timeout;
+bookingApp.animations.handleAnimations = function() {
+    if (!bookingApp.animations.isOn) {
+      bookingApp.animations.init();
+      bookingApp.animations.timeoutTransition(1200);
+    } else {
+      bookingApp.animations.reset();
+    }
+    bookingApp.animations.isOn = !bookingApp.animations.isOn;
+}
+bookingApp.animations.init = function() {
+    bookingApp.animations.createCanvas(bookingApp.recommendationDisplay);
+    bookingApp.animations.appendElements('tomato');
+    bookingApp.recommendationSection.css({
+        'overflow': 'hidden'
+    });
+    console.log(bookingApp.animations.animatedElements[0])
+    bookingApp.animations.animatedElements[0].animate({
+      'transform': 'rotate(0deg)',
+      'opacity': '0'
+    },{
+      'duration': 1500,
+      'step': function (now) {
+        bookingApp.animations.animatedElements[0].css({
+          'transform': `rotate(${now*300}deg)`
+        });
+      }
+    })
+}
+bookingApp.animations.reset = function() {
+    bookingApp.animations.eraseCanvas(bookingApp.animations.canvas);
+}
+bookingApp.animations.isOn = false;
 bookingApp.animations.canvas;
 bookingApp.animations.elements = [];
-bookingApp.animations.createCanvas = function() {
-    $('body').append(`
+bookingApp.animations.createCanvas = function(parentElement) {
+    parentElement.append(`
         <div class="animationCanvas">
         </div> <!-- closing animationCanvas -->
     `);
     bookingApp.animations.canvas = $('.animationCanvas');
 }
-bookingApp.animations.appendElement = function(element) {
-    bookingApp.animations.canvas.append(element);
+bookingApp.animations.eraseCanvas = function(parentElement) {
+    parentElement.html('');
 }
-bookingApp.animations.elements = function(...elements) {
+bookingApp.animations.timeoutTransition = function(timeoutDuration) {
+    bookingApp.animations.timeout = setTimeout(() => {
+      bookingApp.recommendationSection.css({
+        'overflow': 'scroll'
+      });
+      bookingApp.animations.canvas.animate({
+        'width': '0',
+        'height': '0',
+        'top': '50%',
+        'left': '50%'
+      }, 700);
+    }, timeoutDuration);
+}
+bookingApp.animations.appendElements = function(...elements) {
     bookingApp.animations.elements = elements.map( (value) => {
-        return `<img scr="./assets/${value}.png" alt="picture of ${value} id="${value}"/>`;
+        return `<img src="./assets/${value}.png" alt="picture of ${value}" id="${value}">`;
     });
-}
-bookingApp.animations.animate = function(element) {
-    element.animate({
-        'transform': `rotate()`
-    })
+    bookingApp.animations.elements.forEach( (value) => {
+        bookingApp.animations.canvas.append(value);
+    });
+    bookingApp.animations.animatedElements = elements.map( (value) => {
+        return $(`img#${value}`);
+    });
 }
